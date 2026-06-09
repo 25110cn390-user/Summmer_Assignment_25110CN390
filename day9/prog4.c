a34cd4200915637d5890adb0cd1a984c69181b19
Sneha Mishra <25110cn390@glbitm.ac.in>
2026-06-09 14:47:29 +0530

    Add files via upload

---
+++ day9/prog4.c
@@ -0,0 +1,19 @@
+#include<stdio.h>
+//wap to print hollow square pattern
+int main(){
+    int i,j,n;
+    printf("Enter the number of rows: ");
+    scanf("%d",&n);
+    for(i=1;i<=n;i++){
+        for(j=1;j<=n;j++){
+            if(i==1 || i==n || j==1 || j==n){
+                printf("* ");
+            }
+            else{
+                printf("  ");
+            }
+        }
+        printf("\n");
+    }
+    return 0;
+}
