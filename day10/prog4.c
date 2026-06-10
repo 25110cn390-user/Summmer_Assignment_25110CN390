d8f3558ab23341fade2a326eb01a856e47e7a15f
Sneha Mishra <25110cn390@glbitm.ac.in>
2026-06-10 10:25:24 +0530

    Add files via upload

---
+++ day10/prog4.c
@@ -0,0 +1,21 @@
+#include<stdio.h>
+//wap to print character pyramid
+int main(){
+    int i,j,n;
+    char ch='A';
+    printf("Enter the number of rows :");
+    scanf("%d",&n);
+    for(i=1;i<=n;i++){
+        for(j=1;j<=n-i;j++){
+            printf("  ");
+        }
+        for(j=1;j<=i;j++){
+            printf("%c ",j+64);
+        }
+        for(j=i-1;j>=1;j--){
+            printf("%c ",j+64);
+        }
+        printf("\n");
+    }
+    return 0;
+}
